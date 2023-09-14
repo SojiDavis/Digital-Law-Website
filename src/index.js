b3319612e54441a4257556dbda52ee2e968b411f
SojiDavis <sojidavisp@gmail.com>
2023-09-14 21:56:51 +0100

    forth Commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./main-component/App/App";
 import * as serviceWorker from "./serviceWorker";
@@ -9,7 +10,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
